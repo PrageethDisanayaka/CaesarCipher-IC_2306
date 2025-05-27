673552a1423b9ca31515bb8987a535d9cf43be98
Prageeth Disanayaka <153439786+PrageethDisanayaka@users.noreply.github.com>
2025-05-27 22:44:31 +0530

    Initial project setup
    these are the main and header files for this project

---
+++ main.c
@@ -0,0 +1,8 @@
+// This is main c file
+
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include "queue_header.h"
+
+
---
+++ queue_header.h
@@ -0,0 +1,7 @@
+// This is header file
+
+#ifndef QUEUE_H
+#define QUEUE_H
+
+
+#endif

c84805f97d7be621f26930535bb308b6bac41b55
Maduka Sewwandi <161553850+MadukaSewwandi@users.noreply.github.com>
2025-05-28 16:10:48 +0530

    Encryption added

--- main.c
+++ main.c
@@ -5,4 +5,13 @@
 #include <ctype.h>
 #include "queue_header.h"
 
+// Encrypts a message
 
+void encryptMessage(CircularQueue *q, char *message, int key) {
+    printf("\nEncrypted Message: ");
+
+    for (int i = 0; message[i] != '\0'; i++) {
+        printf("%c", shiftChar(q, message[i], key));
+    }
+    printf("\n\n ***** Thank You  .. ******\n");
+}

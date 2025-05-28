60927683fa5ac12a58a2348f6f49a6df1b450a47
Prageeth Disanayaka <153439786+PrageethDisanayaka@users.noreply.github.com>
2025-05-28 11:23:41 +0530

    initialized Circular q

--- main.c
+++ main.c
@@ -6,3 +6,15 @@
 #include "queue_header.h"
 
 
+// Initialize Circular queue
+
+void initAlphabetQueue(CircularQueue *q) {
+    q->front = 0;
+    q->rear = MAXQUEUE - 1;
+    q->count = 0;
+
+    for (int i = 0; i < MAXQUEUE; i++) {
+        q->items[i] = 'A' + i; // Populate the queue's items with alphabet chars
+        q->count++;
+    }
+}
